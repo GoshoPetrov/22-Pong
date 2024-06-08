ccda508b14d6e958307ab97a1311f6aea3611f01
GoshoPetrov <yankrustev@gmail.com>
2024-06-08 15:16:12 +0300

    Add project files.

---
+++ Ball.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pong
+{
+    public class Ball
+    {
+        private readonly char _tile;
+        private readonly int _initialX;
+        private readonly int _initialY;
+
+        private int _x;
+        private int _y;
+        private int _dx;
+        private int _dy;
+
+        public Ball(int x, int y, char tile = '0')
+        {
+            this._tile = tile;
+
+            this._x = this._initialX = x;
+            this._y = this._initialY = y;
+
+            this._dx = 1;
+            this._dy = 1;
+        }
+
+        public int Y => this._y;
+        public int X => this._x;
+
+        public void Draw(Field field)
+        {
+            field.Set(row: this._x, col: this._y, this._tile);
+        }
+
+        public void CalculateTrajectory(Field field, char racketTile)
+        {
+            field.Set(row: this._x, col: this._y, ' ');
+
+            char place = field.Get(row: this._x + this._dx, col: this._y + this._dy);
+            if (place == field.Tile)
+            {
+                this._dx *= -1;
+            }
+
+            if (place == racketTile)
+            {
+                this._dy *= -1;
+            }
+
+            this._x += this._dx;
+            this._y += this._dy;
+
+            this.Draw(field);
+
+        }
+
+        public void Reset(Field field)
+        {
+            field.Set(row: this._x, col: this._y, value: ' ');
+            this._x = this._initialX;
+            this._y = this._initialY;
+            this.Draw(field);
+        }
+    }
+}
---
+++ Racket.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pong
+{
+    public class Racket
+    {
+        private readonly int _y;
+        private readonly int _length;
+
+        private readonly int _initialX;
+
+        private int _x;
+
+        public Racket(int x, int y, int length = 5, char tile = '|')
+        {
+            this._x = this._initialX = x;
+            this._y = y;
+            this._length = length;
+            this.Tile = tile;
+        }
+        public char Tile { get; }
+
+        public void Draw(Field field)
+        {
+            for (int i = 0; i < this._length; i++)
+            {
+                field.Set(row: i + this._x, col: this._y, this.Tile);
+            }
+        }
+
+        public void MoveUp(Field field)
+        {
+            if (field.Get(row: this._x -1, col: this._y) == field.Tile)
+            {
+                return;
+            }
+            field.Set(row: this._x + (this._length - 1), col: this._y, value: ' ');
+            this._x -= 1;
+            field.Set(row: this._x, col: this._y, value: this.Tile);
+        }
+
+        public void MoveDown(Field field)
+        {
+            if (field.Get(row: this._x + this._length, col: this._y) == field.Tile)
+            {
+                return;
+            }
+            field.Set(row: this._x, col: this._y, value: ' ');
+            this._x += 1;
+            field.Set(row: this._x + (this._length - 1), col: this._y, value: this.Tile);
+        }
+
+        public void Reset(Field field)
+        {
+            for (int i=0; i<this._length; i++)
+            {
+                field.Set(row: i + this._x, col: this._y, value: ' ');
+            }
+
+            this._x = this._initialX;
+            this.Draw(field);
+        }
+    }
+}
